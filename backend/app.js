f892d1b61d83de0f8adf7daf45dbab55f6e1e049
Govind Magar <govind.magar21@gmail.com>
2023-06-25 21:28:32 +0530

    Changed proxy to CORS

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,12 @@ const express = require("express");
 const app = express();
 const connectToDB = require("./config/db");
 const userRouter = require("./routes/userRoutes");
+const cors = require("cors");
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors());
 
 connectToDB();
 app.use("/", userRouter);
