f79d6ff613af21c574261706669ce76bef1ccbb7
Govind Magar <govind.magar21@gmail.com>
2023-06-25 23:32:53 +0530

    updated host URL

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
 
-const BASE_URL = "https://crud-application-kappa.vercel.app"
+const BASE_URL = "https://crud-application-fawn.vercel.app"
 
 export const Form = () => {
   // To Store the value from Frontend
