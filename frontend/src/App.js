01af36f3b123d655981bd922e34f6a7fa299ee3e
Govind Magar <govind.magar21@gmail.com>
2023-06-25 22:24:39 +0530

    Updated dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { Form } from "./components/Form";
 import { UserList } from "./components/UserList";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <div>
       <Form />
       <UserList />
+      <Toaster />
     </div>
   );
 }
