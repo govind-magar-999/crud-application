653b86c2030b1101990ebf8408a344371015a3b5
Govind Magar <govind.magar21@gmail.com>
2023-06-26 00:21:57 +0530

    Adjusted placement of dependency declaration

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,16 @@
 require("dotenv").config;
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const connectToDB = require("./config/db");
 const userRouter = require("./routes/userRoutes");
-const cors = require("cors");
+
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
 app.use(cors());
 
 connectToDB();
